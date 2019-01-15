a2a44f646d1fa47b41a9c5e984dff41f7b3a45ba
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2019-01-14 20:40:55 -0800

    Updates

---
+++ precache-manifest.76c363c1d13ade8384010b5c1d4aeb87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6554192b8a38e69bd924",
+    "url": "/stock-chart-client/static/js/runtime~main.6554192b.js"
+  },
+  {
+    "revision": "47db4a5f32ddd2289a89",
+    "url": "/stock-chart-client/static/js/main.47db4a5f.chunk.js"
+  },
+  {
+    "revision": "921d051c8c4bcb833027",
+    "url": "/stock-chart-client/static/js/1.921d051c.chunk.js"
+  },
+  {
+    "revision": "47db4a5f32ddd2289a89",
+    "url": "/stock-chart-client/static/css/main.ae570774.chunk.css"
+  },
+  {
+    "revision": "921d051c8c4bcb833027",
+    "url": "/stock-chart-client/static/css/1.6c0f2eb5.chunk.css"
+  },
+  {
+    "revision": "68b026e23fa2b5b79a9e8d31d1db7aa3",
+    "url": "/stock-chart-client/index.html"
+  }
+];
