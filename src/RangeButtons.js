88cd45f7229372bd166c89b09c5d4ca89f713225
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2019-01-14 19:43:20 -0800

    Refactor range buttons

--- src/RangeButtons.js
+++ src/RangeButtons.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Button, ButtonGroup } from "@progress/kendo-react-buttons";
 
+import "./RangeButtons.css";
+
 class ButtonGroupContainer extends Component {
   constructor(props) {
     super(props);
@@ -14,32 +16,30 @@ class ButtonGroupContainer extends Component {
     const { value } = this.props;
 
     return (
-      <div className="row">
-        <div className="col-xs-12 col-sm-6 example-col">
-          <ButtonGroup>
-            <Button
-              togglable={true}
-              selected={value === "1m"}
-              onClick={this.handleClickOneMonth}
-            >
-              1 month
-            </Button>
-            <Button
-              togglable={true}
-              selected={value === "6m"}
-              onClick={this.handleClickSixMonths}
-            >
-              6 months
-            </Button>
-            <Button
-              togglable={true}
-              selected={value === "1y"}
-              onClick={this.handleClickOneYear}
-            >
-              1 year
-            </Button>
-          </ButtonGroup>
-        </div>
+      <div className="RangeButtons">
+        <ButtonGroup>
+          <Button
+            togglable={true}
+            selected={value === "1m"}
+            onClick={this.handleClickOneMonth}
+          >
+            1 month
+          </Button>
+          <Button
+            togglable={true}
+            selected={value === "6m"}
+            onClick={this.handleClickSixMonths}
+          >
+            6 months
+          </Button>
+          <Button
+            togglable={true}
+            selected={value === "1y"}
+            onClick={this.handleClickOneYear}
+          >
+            1 year
+          </Button>
+        </ButtonGroup>
       </div>
     );
   }
