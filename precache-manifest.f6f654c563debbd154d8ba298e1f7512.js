8853b99c74416421f71fa7a1bbd57ea6f7392403
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2019-01-14 20:56:55 -0800

    Updates

--- precache-manifest.f6f654c563debbd154d8ba298e1f7512.js
+++ precache-manifest.f6f654c563debbd154d8ba298e1f7512.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/stock-chart-client/static/js/runtime~main.6554192b.js"
   },
   {
-    "revision": "47db4a5f32ddd2289a89",
-    "url": "/stock-chart-client/static/js/main.47db4a5f.chunk.js"
+    "revision": "21d48cf57ec30ea7f5a5",
+    "url": "/stock-chart-client/static/js/main.21d48cf5.chunk.js"
   },
   {
     "revision": "921d051c8c4bcb833027",
     "url": "/stock-chart-client/static/js/1.921d051c.chunk.js"
   },
   {
-    "revision": "47db4a5f32ddd2289a89",
+    "revision": "21d48cf57ec30ea7f5a5",
     "url": "/stock-chart-client/static/css/main.ae570774.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/stock-chart-client/static/css/1.6c0f2eb5.chunk.css"
   },
   {
-    "revision": "68b026e23fa2b5b79a9e8d31d1db7aa3",
+    "revision": "570d23401b2e68c352630ef559c4d760",
     "url": "/stock-chart-client/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stock-chart-client/precache-manifest.76c363c1d13ade8384010b5c1d4aeb87.js"
+  "/stock-chart-client/precache-manifest.f6f654c563debbd154d8ba298e1f7512.js"
 );
 
 workbox.clientsClaim();
