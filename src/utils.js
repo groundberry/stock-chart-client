7b45ea9bb6194c13256f52a8c7d1284526bdba03
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2019-01-28 16:37:15 -0800

    Refactor fetchData function

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,18 @@
 const serverUrl = "https://stock-chart-proxy.herokuapp.com/";
 
-export function fetchData(symbol, range) {
+export async function fetchData(symbol, range) {
   const url = `${serverUrl}?symbol=${symbol}&range=${range}`;
 
-  return fetch(url).then(response => response.json());
+  const response = await fetch(url);
+  return response.json();
 }
 
 export function massageData(obj) {
   return {
-    Date: `/Date(${new Date(obj.date).getTime()})/`,
-    Close: obj.close,
-    Volume: obj.volume,
+    Date: new Date(obj.date),
     Open: obj.open,
+    Close: obj.close,
+    Low: obj.low,
     High: obj.high,
-    Low: obj.low
   };
 }
